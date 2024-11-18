254fcade88edfdde3d8555b8242ac65603a5fc89
Kazbek256 <kazbek.hashimov@gmail.com>
2024-11-18 21:16:12 +0000

    script

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-print("{}".format("".join([chr(i) for i in range(ord('a'), ord('z') + 1)
-                           if chr(i) != 'q' and chr(i) != 'e'])))
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print(chr(i), end="")
+
 
 
 
